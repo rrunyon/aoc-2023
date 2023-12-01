887cf817e8a8129c4dbffb0be27a8dd62d3db34d
Rick Runyon <rick.runyon@gmail.com>
2023-12-01 06:25:33 -0500

    1.1

---
+++ 01/1.js
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+
+function solution() {
+  let input = fs.readFileSync('./01/input.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
+  let sum = 0;
+
+  for (let line of input) {
+    if (!line) continue;
+
+    line = line.replaceAll(/[a-z]*/gi, '')
+    console.log(line);
+    sum += parseInt(line[0] + line[line.length - 1]);
+  }
+
+  return sum;
+}
+
+console.log(solution());
