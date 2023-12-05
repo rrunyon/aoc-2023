e1b85e441dd0ad9f4b6dfff8c7e8f76b62e71544
Rick Runyon <rick.runyon@gmail.com>
2023-12-04 21:25:37 -0500

    Only count winners for each card once

--- 04/2.js
+++ 04/2.js
@@ -2,32 +2,40 @@ import * as fs from 'fs';
 
 function solution() {
   let input = fs.readFileSync('./04/input.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
-  return countWinners(input);
+
+  let winningCountMap = new Map;
+  for (let i = 0; i < input.length; i++) {
+    let row = input[i];
+    let [left, right] = parseRow(row);
+    let winningNumbers = new Set(left);
+    let winningNumberCount = 0;
+
+    for (let number of right) {
+      if (winningNumbers.has(number)) winningNumberCount++;
+    }
+
+    winningCountMap.set(i, winningNumberCount);
+  }
+
+  return countWinners(input, winningCountMap);
 }
 
-function countWinners(cards, index = 0, depth = 0) {
+function countWinners(cards, winningCountMap, index = 0, depth = 0) {
   if (index >= cards.length) return 0;
 
-  let row = cards[index];
-  let [left, right] = parseRow(row);
-  let winningNumbers = new Set(left);
-  let winningNumberCount = 0;
-
-  for (let number of right) {
-    if (winningNumbers.has(number)) winningNumberCount++;
-  }
+  let winningNumberCount = winningCountMap.get(index);
 
   let count = 1;
   if (winningNumberCount > 0) {
     for (let i = 1; i <= winningNumberCount; i++) {
-      count += countWinners(cards, index + i, depth + 1);
+      count += countWinners(cards, winningCountMap, index + i, depth + 1);
     }
   }
 
   if (depth > 0) {
     return count;
   } else {
-    return count + countWinners(cards, index + 1);
+    return count + countWinners(cards, winningCountMap, index + 1);
   }
 }
 
